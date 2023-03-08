7839b9d4b55e01579b0a067369d420e234a6afb8
ech0r <ech0r@thinkpad>
2023-03-07 20:09:34 -0800

    updated slstatus

--- slstatus/config.h
+++ slstatus/config.h
@@ -65,7 +65,12 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-    { battery_remaining, "%s", "BAT0" },
-    { battery_perc, " %s%% ",   "BAT0" }, 
-	{ datetime, "%s",           "%F %T" },
+    { wifi_essid,           "%s |","wlp5s0" }, 
+    { wifi_perc,           " %s%% |","wlp5s0" }, 
+    { netspeed_tx,           " %s |","wlp5s0" }, 
+    { netspeed_rx,           " %s |","wlp5s0" }, 
+    { ram_used,           " %s |",NULL }, 
+    { battery_remaining," %s |","BAT0" },
+    { battery_perc, " %s%% |","BAT0" }, 
+	{ datetime,        " %s ","%F | %T" },
 };
